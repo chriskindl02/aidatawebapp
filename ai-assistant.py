11722df554d8fec6955b8c9695d566989afcc6a8
chriskindl02 <chriskindl@live.de>
2024-02-15 00:07:28 +0100

    first ai try

--- ai-assistant.py
+++ ai-assistant.py
@@ -5,23 +5,26 @@ from streamlit_chat import message
 import numpy as np
 import pandas as pd
 
+main_data="empty"
 
+st.title("Echo Bot")
 
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
 
 with st.chat_message("user"):
-    st.write("Hello 👋")
-    st.write("Hello human")
-    uploaded_file = st.file_uploader("Bitte zu analysierende Datei hochladen",type=['xlsx','csv'],accept_multiple_files=False)
-    main_data = "No Data selected"
+    st.write("Hallo Nutzer👋")
+    uploaded_file = st.file_uploader("Bitte lade die Datei hoch, die ich für dich analysieren soll:",type=['xlsx','csv'],accept_multiple_files=False)
+    
     if uploaded_file is not None:
-        st.write(uploaded_file.name)
         xls = pd.ExcelFile(uploaded_file)
         sheet_names = xls.sheet_names
-        selected_sheet=st.selectbox("select the sheet", sheet_names)
+        selected_sheet=st.selectbox("Bitte wähle das Tabellenblatt aus, welches ich für dich analysieren soll!", sheet_names)
         df = pd.read_excel (uploaded_file, sheet_name=selected_sheet)
-        main_data = df
-
-    st.write(main_data)
+        main_data =df
+        st.write (main_data)
         
-    
+prompt = st.chat_input("Say something")
+if prompt:
+    st.write(f"User has sent the following prompt: {prompt}")
